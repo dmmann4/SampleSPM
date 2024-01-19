a3242b945fdfb1e6082c3b3f7affcebe41ece403
David Mann <david.mann@magneticmobile.com>
2024-01-19 10:55:47 -0500

    init commit

--- Sources/SampleSPM/SampleSPM.swift
+++ Sources/SampleSPM/SampleSPM.swift
@@ -1,2 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public class SampleSPM {
+  public func changeIntValue() -> Int {
+    return Int.random(in: 1..<100)
+  }
+}
